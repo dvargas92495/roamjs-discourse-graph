316d13c6a6876d4c8a3dd146ef6a656381a91596
David Vargas <dvargas92495@gmail.com>
2022-05-18 13:00:14 -0500

    fix worker file loading

--- src/dataWorkerClient.ts
+++ src/dataWorkerClient.ts
@@ -24,18 +24,20 @@ const loadGraph = (configUid: string, update = false) =>
     const closeLoadingToast: { current?: () => void } = {
       current: undefined,
     };
-    const cachePath = getSettingValueFromTree({
+    const cachePathNodes = getSubTree({
       tree: getBasicTreeByParentUid(configUid),
       key: "cache",
-    });
+    }).children;
+    const opts = {
+      headers: { Authorization: getAuthorizationHeader() },
+    };
     listeners["init"] = ({ graph }: { graph?: string }) => {
       delete listeners["init"];
       dataWorker.init = true;
       document.body.dispatchEvent(new Event("roamjs:data-worked:init"));
-      closeLoadingToast.current?.();
       if (graph) {
-        (cachePath
-          ? Promise.resolve(cachePath)
+        (cachePathNodes.length
+          ? Promise.resolve(cachePathNodes[0]?.text)
           : Promise.resolve(v4()).then((value) =>
               setInputSetting({
                 blockUid: configUid,
@@ -44,93 +46,116 @@ const loadGraph = (configUid: string, update = false) =>
               }).then(() => value)
             )
         )
-          .then((path) =>
-            axios.post("https://lambda.roamjs.com/file", {
-              dev: process.env.NODE_ENV === "development",
-              extension: "discourse-graph",
-              body: graph,
-              path,
-            })
+          .then((cachePath) =>
+            axios.put(
+              "https://lambda.roamjs.com/file",
+              {
+                dev: process.env.NODE_ENV === "development",
+                extension: "discourse-graph",
+                body: graph,
+                path: `graph-cache/${cachePath}.json`,
+              },
+              opts
+            )
           )
-          .then(() =>
+          .then(() => {
+            closeLoadingToast.current?.();
             renderToast({
-              id: "localstorage-success",
+              id: "storage-success",
               content: `Successfully stored discourse graph cache!`,
               intent: Intent.SUCCESS,
-            })
-          )
+            });
+          })
           .catch((e) =>
             renderToast({
-              id: "localstorage-error",
+              id: "storage-error",
               content: `Unkown error: ${e.message}`,
               intent: Intent.DANGER,
             })
           );
       } else {
+        closeLoadingToast.current?.();
         renderToast({
-          id: "localstorage-success",
+          id: "storage-success",
           content: `Successfully loaded graph from cache!`,
           intent: Intent.SUCCESS,
         });
       }
       resolve();
     };
+    const loadCache = () =>
+      new Promise((innerResolve) => {
+        loadingRender({
+          operation: () => {
+            const blocks = window.roamAlphaAPI.q(`[:find 
+          (pull ?b 
+            [
+              :db/id 
+              :node/title 
+              :block/string 
+              :block/page 
+              :block/refs 
+              :block/uid 
+              :block/children 
+              [:create/time :as "createdTime"] 
+              [:edit/time :as "editedTime"]
+              [:user/display-name :as "displayName"]
+              [:create/user :as "createdBy"]
+            ]
+          ) 
+          :where ${
+            update
+              ? `[?b :edit/time ?t] [(< ${localStorage.getItem(
+                  "graph-timestamp"
+                )} ?t)]`
+              : "[?b :block/uid]"
+          }]`);
+            innerResolve(blocks);
+          },
+          content: "Please wait as we load your graph's discourse data...",
+        });
+      });
     return (
-      cachePath
+      cachePathNodes.length
         ? axios
             .get(
               `https://lambda.roamjs.com/file?extension=discourse-graph${
                 process.env.NODE_ENV === "development" ? "&dev=true" : ""
-              }&path=graph-cache/${cachePath}.json`,
-              {
-                headers: { Authorization: getAuthorizationHeader() },
-              }
+              }&path=graph-cache/${cachePathNodes[0]?.text}.json`,
+              opts
             )
             .then((r) => r.data)
-        : new Promise((innerResolve) => {
-            loadingRender({
-              operation: () => {
-                const blocks = window.roamAlphaAPI.q(`[:find 
-                (pull ?b 
-                  [
-                    :db/id 
-                    :node/title 
-                    :block/string 
-                    :block/page 
-                    :block/refs 
-                    :block/uid 
-                    :block/children 
-                    [:create/time :as "createdTime"] 
-                    [:edit/time :as "editedTime"]
-                    [:user/display-name :as "displayName"]
-                    [:create/user :as "createdBy"]
-                  ]
-                ) 
-                :where ${
-                  update
-                    ? `[?b :edit/time ?t] [(< ${localStorage.getItem(
-                        "graph-timestamp"
-                      )} ?t)]`
-                    : "[?b :block/uid]"
-                }]`);
-                innerResolve(blocks);
-              },
-              content: "Please wait as we load your graph's discourse data...",
-            });
-          })
-    ).then((blocks) => {
-      closeLoadingToast.current = renderToast({
-        id: "dataworker-loading",
-        content: `Graph is continuing to build in the background...`,
-        intent: Intent.PRIMARY,
-        position: Position.BOTTOM_RIGHT,
-        timeout: 0,
+            .catch((e) => {
+              if (e.response?.status === 404) {
+                return Promise.all(
+                  cachePathNodes.map((n) => deleteBlock(n.uid))
+                ).then(loadCache);
+              } else {
+                throw e;
+              }
+            })
+        : loadCache()
+    )
+      .then((blocks) => {
+        closeLoadingToast.current = renderToast({
+          id: "dataworker-loading",
+          content: `Graph is continuing to build in the background...`,
+          intent: Intent.PRIMARY,
+          position: Position.BOTTOM_RIGHT,
+          timeout: 0,
+        });
+        dataWorker.current.postMessage({
+          method: "init",
+          blocks,
+        });
+      })
+      .catch((e) => {
+        renderToast({
+          id: "load-graph",
+          content: `Unkown error: ${e.message}`,
+          intent: Intent.DANGER,
+        });
       });
-      dataWorker.current.postMessage({
-        method: "init",
-        blocks,
-      });
-    });
   });
 
 export const refreshUi: { [k: string]: () => void } = {};
