58a69515e2e76cbb6b36f63e9b57c24d8badb38d
David Vargas <dvargas92495@gmail.com>
2022-10-16 23:13:09 -0400

    Nuke BlockFeed and Subscription Panel

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,11 @@
 import createBlock from "roamjs-components/writes/createBlock";
-import createHTMLObserver from "roamjs-components/dom/createHTMLObserver";
 import getChildrenLengthByPageUid from "roamjs-components/queries/getChildrenLengthByPageUid";
-import getCurrentPageUid from "roamjs-components/dom/getCurrentPageUid";
-import getPageTitleByHtmlElement from "roamjs-components/dom/getPageTitleByHtmlElement";
-import getPageTitleByPageUid from "roamjs-components/queries/getPageTitleByPageUid";
 import getTextByBlockUid from "roamjs-components/queries/getTextByBlockUid";
 import runExtension from "roamjs-components/util/runExtension";
-import toConfig from "roamjs-components/util/toConfigPageName";
 import updateBlock from "roamjs-components/writes/updateBlock";
-import toFlexRegex from "roamjs-components/util/toFlexRegex";
 import { render as renderToast } from "roamjs-components/components/Toast";
 import { render as importRender } from "./ImportDialog";
-import { render as notificationRender } from "./NotificationIcon";
 import { render as queryRequestRender } from "./components/SendQueryRequest";
-import { render as renderBlockFeed } from "./components/BlockFeed";
-import {
-  getUserIdentifier,
-} from "./util";
-import ReactDOM from "react-dom";
 import importDiscourseGraph from "./utils/importDiscourseGraph";
 import { Intent } from "@blueprintjs/core";
 import createButtonObserver from "roamjs-components/dom/createButtonObserver";
@@ -29,9 +17,6 @@ import getSamePageApi from "@samepage/external/getSamePageAPI";
 import apiPost from "roamjs-components/util/apiPost";
 import getBasicTreeByParentUid from "roamjs-components/queries/getBasicTreeByParentUid";
 import getSubTree from "roamjs-components/util/getSubTree";
-
-const CONFIG = toConfig("discourse-graph");
-const user = getUserIdentifier();
 
 const extensionId = "discourse-graph";
 
@@ -227,108 +212,6 @@ Click on the ✖️ to dismiss for good (you won't see this message again).`,
           }
         };
       },
-    });
-
-    const elToTitle = (e: Node): string => {
-      if (e.nodeName === "#text") {
-        return e.nodeValue;
-      } else if (
-        e.nodeName === "SPAN" &&
-        (e as HTMLSpanElement).classList.contains("rm-page-ref__brackets")
-      ) {
-        return "";
-      } else if (
-        e.nodeName === "SPAN" &&
-        (e as HTMLSpanElement).classList.contains("rm-page-ref")
-      ) {
-        return `[[${Array.from(e.childNodes).map(elToTitle).join("")}]]`;
-      } else {
-        return Array.from(e.childNodes).map(elToTitle).join("");
-      }
-    };
-
-    const clearOnClick = (tag: string) => {
-      const uid = window.roamAlphaAPI.ui.getFocusedBlock()?.["block-uid"] || "";
-      const text = `[[${tag}]]`;
-      if (uid) {
-        const currentText = getTextByBlockUid(uid);
-        setTimeout(
-          () =>
-            updateBlock({
-              text: `${currentText} ${text}`,
-              uid,
-            }),
-          1
-        );
-      } else {
-        const parentUid = getCurrentPageUid();
-        const order = getChildrenLengthByPageUid(parentUid);
-        createBlock({ parentUid, node: { text }, order });
-      }
-    };
-
-    const getSubscribedBlocks = () =>
-      //treeRef.tree
-      getBasicTreeByParentUid(
-        getPageUidByPageTitle("roam/js/discourse-graph")
-      ).find((s) => toFlexRegex("subscriptions").test(s.text))?.children || [];
-
-    const showNotificationIcon = (url: string) => {
-      const subscribedBlocks = getSubscribedBlocks();
-      const subscribedUids = new Set(
-        subscribedBlocks.map((t) => t.children[0]?.children?.[0]?.text)
-      );
-      const uid = url.match(/\/page\/(.*)$/)?.[1] || "";
-      if (uid && subscribedUids.has(uid)) {
-        const article = document.querySelector<HTMLDivElement>(".roam-article");
-        const articleStyle = getComputedStyle(article);
-        const span = document.createElement("span");
-        span.style.position = "absolute";
-        span.style.top = articleStyle.paddingTop;
-        span.style.left = articleStyle.paddingLeft;
-        span.id = "roamjs-discourse-notification-icon";
-        setTimeout(() => {
-          article.insertBefore(span, article.firstElementChild);
-          const notificationBlock = (
-            subscribedBlocks.find((t) => toFlexRegex(user).test(t.text))
-              .children || []
-          ).find((t) => t.children[0].text === uid);
-          const defaultTimestamp = new Date().valueOf();
-          (notificationBlock.children[1]?.uid
-            ? Promise.resolve(notificationBlock.children[1]?.uid)
-            : createBlock({
-                node: { text: `${defaultTimestamp}` },
-                parentUid: notificationBlock.uid,
-                order: 1,
-              })
-          ).then((configUid) =>
-            notificationRender({
-              p: span,
-              parentUid: uid,
-              timestamp:
-                Number(notificationBlock.children[1]?.text) || defaultTimestamp,
-              configUid,
-            })
-          );
-        }, 1000);
-      }
-    };
-
-    window.addEventListener("hashchange", (e) => {
-      const oldIcon = document.getElementById(
-        "roamjs-discourse-notification-icon"
-      );
-      if (oldIcon) {
-        ReactDOM.unmountComponentAtNode(oldIcon);
-        oldIcon.remove();
-      }
-      showNotificationIcon(e.newURL);
-    });
-    showNotificationIcon(window.location.hash);
-
-    window.roamAlphaAPI.ui.commandPalette.addCommand({
-      label: "Open Feed",
-      callback: () => renderBlockFeed({}),
     });
   },
 });
