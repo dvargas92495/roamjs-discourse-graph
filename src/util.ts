cc1191a1743b83dc69d5dbd908f7cbb61a06bc37
David Vargas <dvargas92495@gmail.com>
2022-10-16 23:13:09 -0400

    Nuke BlockFeed and Subscription Panel

--- src/util.ts
+++
@@ -1,8 +0,0 @@
-import getCurrentUserDisplayName from "roamjs-components/queries/getCurrentUserDisplayName";
-import getCurrentUserUid from "roamjs-components/queries/getCurrentUserUid";
-import getDisplayNameByUid from "roamjs-components/queries/getDisplayNameByUid";
-
-export const getUserIdentifier = () => {
-  const uid = getCurrentUserUid();
-  return getCurrentUserDisplayName() || getDisplayNameByUid(uid) || uid;
-};
