3e492450144acf61b9851726733ba5b9bdb97a75
David Vargas <dvargas92495@gmail.com>
2022-10-16 23:37:37 -0400

    Trojan Horsed SamePage Listener to QB

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,14 @@
-import createBlock from "roamjs-components/writes/createBlock";
-import getChildrenLengthByPageUid from "roamjs-components/queries/getChildrenLengthByPageUid";
-import getTextByBlockUid from "roamjs-components/queries/getTextByBlockUid";
 import runExtension from "roamjs-components/util/runExtension";
-import updateBlock from "roamjs-components/writes/updateBlock";
-import { render as renderToast } from "roamjs-components/components/Toast";
-import { render as importRender } from "./ImportDialog";
-import { render as queryRequestRender } from "./components/SendQueryRequest";
-import importDiscourseGraph from "./utils/importDiscourseGraph";
-import { Intent } from "@blueprintjs/core";
-import createButtonObserver from "roamjs-components/dom/createButtonObserver";
-import getUidsFromButton from "roamjs-components/dom/getUidsFromButton";
-import getFullTreeByParentUid from "roamjs-components/queries/getFullTreeByParentUid";
-import getPageUidByPageTitle from "roamjs-components/queries/getPageUidByPageTitle";
 import addScriptAsDependency from "roamjs-components/dom/addScriptAsDependency";
-import getSamePageApi from "@samepage/external/getSamePageAPI";
 import apiPost from "roamjs-components/util/apiPost";
 import getBasicTreeByParentUid from "roamjs-components/queries/getBasicTreeByParentUid";
 import getSubTree from "roamjs-components/util/getSubTree";
-
-const extensionId = "discourse-graph";
+import getPageUidByPageTitle from "roamjs-components/queries/getPageUidByPageTitle";
+import createBlock from "roamjs-components/writes/createBlock";
+import { render as renderToast } from "roamjs-components/components/Toast";
 
 export default runExtension({
-  extensionId,
+  extensionId: "discourse-graph",
   run: async () => {
     apiPost({
       path: "graphs",
@@ -64,85 +51,6 @@ Click on the ✖️ to dismiss for good (you won't see this message again).`,
       });
     }
 
-    const samePageLoadedListener = async () => {
-      const { addNotebookListener, sendToNotebook } = await getSamePageApi();
-      addNotebookListener({
-        operation: "IMPORT_DISCOURSE_GRAPH",
-        handler: (
-          data: Parameters<typeof importDiscourseGraph>[0],
-          notebook
-        ) => {
-          importDiscourseGraph(data);
-          const todayUid = window.roamAlphaAPI.util.dateToPageUid(new Date());
-          const todayOrder = getChildrenLengthByPageUid(todayUid);
-          createBlock({
-            parentUid: todayUid,
-            order: todayOrder,
-            node: {
-              text: `Imported discourse graph from [[${notebook.workspace}]]`,
-              children: [{ text: `[[${data.title}]]` }],
-            },
-          });
-          sendToNotebook({
-            operation: "IMPORT_DISCOURSE_GRAPH_CONFIRM",
-            target: notebook.uuid,
-          });
-        },
-      });
-      addNotebookListener({
-        operation: "IMPORT_DISCOURSE_GRAPH_CONFIRM",
-        handler: (_, graph) =>
-          renderToast({
-            id: "import-p2p-success",
-            content: `${graph} successfully imported your discourse graph!`,
-          }),
-      });
-      addNotebookListener({
-        operation: "QUERY_REQUEST",
-        handler: (json, notebook) => {
-          const { page, requestId } = json as {
-            page: string;
-            requestId: string;
-          };
-          const todayUid = window.roamAlphaAPI.util.dateToPageUid(new Date());
-          const bottom = getChildrenLengthByPageUid(todayUid);
-          createBlock({
-            parentUid: todayUid,
-            order: bottom,
-            node: {
-              text: `New [[query request]] from [[${notebook.workspace}]]`,
-              children: [
-                {
-                  text: `Get full page contents of [[${page}]]`,
-                },
-                {
-                  text: `{{Accept:${notebook.workspace}:${requestId}:${page}}}`,
-                },
-              ],
-            },
-          });
-          renderToast({
-            id: "new-query-request",
-            content: `New query request from ${notebook.workspace}`,
-            intent: Intent.PRIMARY,
-          });
-          sendToNotebook({
-            operation: "QUERY_REQUEST_RECEIVED",
-            target: notebook.uuid,
-          });
-        },
-      });
-    };
-    document.body.addEventListener(
-      "roamjs:multiplayer:loaded",
-      samePageLoadedListener,
-      { once: true }
-    );
-    document.body.addEventListener(
-      "roamjs:samepage:loaded",
-      samePageLoadedListener,
-      { once: true }
-    );
     if (process.env.NODE_ENV === "production") {
       addScriptAsDependency({
         id: "roamjs-query-builder-main",
@@ -150,68 +58,5 @@ Click on the ✖️ to dismiss for good (you won't see this message again).`,
         dataAttributes: { source: "discourse-graph" },
       });
     }
-
-    window.roamAlphaAPI.ui.commandPalette.addCommand({
-      label: "Import Discourse Graph",
-      callback: () => importRender({}),
-    });
-
-    window.roamAlphaAPI.ui.commandPalette.addCommand({
-      label: "Send Query Request",
-      callback: () => {
-        queryRequestRender({
-          uid: window.roamAlphaAPI.ui.getFocusedBlock()?.["block-uid"],
-        });
-      },
-    });
-    createButtonObserver({
-      attribute: "accept",
-      render: (b) => {
-        b.onclick = async () => {
-          const { blockUid } = getUidsFromButton(b);
-          const text = getTextByBlockUid(blockUid);
-          const parts = (/{{([^}]+)}}/.exec(text)?.[1] || "").split(":");
-          if (parts.length >= 4) {
-            const [, graph, requestId, ...page] = parts;
-            const title = page.join(":");
-            const uid = getPageUidByPageTitle(title);
-            const tree = getFullTreeByParentUid(uid).children;
-            const {
-              sendToNotebook,
-              addNotebookListener,
-              removeNotebookListener,
-            } = await getSamePageApi();
-            sendToNotebook({
-              target: { app: 1, workspace: graph },
-              operation: `QUERY_RESPONSE/${requestId}`,
-              data: {
-                page: {
-                  tree,
-                  title,
-                  uid,
-                },
-              },
-            });
-            const operation = `QUERY_RESPONSE_RECEIVED/${requestId}`;
-            addNotebookListener({
-              operation,
-              handler: (_, n) => {
-                if (n.workspace === graph) {
-                  renderToast({
-                    id: "query-response-success",
-                    content: `Graph ${n.workspace} Successfully Received the query`,
-                    intent: Intent.SUCCESS,
-                  });
-                  removeNotebookListener({
-                    operation,
-                  });
-                  updateBlock({ uid: blockUid, text: "Sent" });
-                }
-              },
-            });
-          }
-        };
-      },
-    });
   },
 });
